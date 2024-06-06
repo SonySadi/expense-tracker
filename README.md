2dbee8b4781f0b2c61731d30b9bc6ab31ed72fb2
Sony Sadi <sony.sadi@gmail.com>
2024-06-06 10:02:12 +0600

    fix: Expense Tracker structure added

--- README.md
+++ README.md
@@ -2,6 +2,10 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
+Requirement
+
+- Node v18.17.0
+
 First, run the development server:
 
 ```bash
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,82 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <header className="flex items-center justify-between mx-10 py-2 mb-6">
+        <h1 className="font-bold text-xl">Expense Tracker</h1>
+      </header>
+      <main className="flex min-h-screen flex-col mx-10">
+        <section className="mb-6">
+          <div className="font-bold">
+            <h3>Your Balance</h3>
+            <h1 className="text-3xl">$0.00</h1>
+          </div>
+        </section>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <section className="flex justify-around w-full bg-white shadow-md mb-6">
+          <div className="p-6 text-center">
+            <h3>Income</h3>
+            <p className="text-green-500 font-bold">$0.00</p>
+          </div>
+          <div className="border-l-2 border-gray-300 my-2"></div>
+          <div className="p-6 text-center">
+            <h3>Expense</h3>
+            <p className="text-red-700 font-bold">$0.00</p>
+          </div>
+        </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <section className="w-full mb-6">
+          <h3 className="font-bold border-b-2 border-gray-300 mb-3 py-2">
+            History
+          </h3>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <ul>
+            {Array.from({ length: 3 }).map((_, index) => (
+              <li
+                className="flex justify-between bg-white p-2 shadow-md mb-2 border-r-4 border-red-700"
+                key={index}
+              >
+                <h4>Income</h4>
+                <p>+$0.00</p>
+              </li>
+            ))}
+            <li className="flex justify-between bg-white p-2 shadow-md mb-2 border-r-4 border-green-500">
+              <h4>Income</h4>
+              <p>+$0.00</p>
+            </li>
+          </ul>
+        </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <section className="w-full mb-6">
+          <h3 className="font-bold border-b-2 border-gray-300 mb-3 py-2">
+            Add new transaction
+          </h3>
+          <form>
+            <div className="mb-3">
+              <label className="block py-2">Text</label>
+              <input
+                type="text"
+                className="w-full p-2 border border-gray-300 bg-white  shadow-sm"
+                placeholder="Enter text..."
+              />
+            </div>
+            <div className="mb-3">
+              <label className="block">Amount</label>
+              <p className="text-xs text-gray-500 mb-2">
+                (negative - expense, positive - income)
+              </p>
+              <input
+                type="number"
+                className="w-full p-2 border border-gray-300 bg-white shadow-sm"
+                placeholder="Enter amount..."
+              />
+            </div>
+            <button className="w-full bg-blue-500 text-white p-2">
+              Add transaction
+            </button>
+          </form>
+        </section>
+      </main>
+    </>
   );
 }
